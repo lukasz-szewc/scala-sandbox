6f58bf814c9f830c071387c09c7613eb3481342c
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-04-11 21:13:10 +0200

    gropu by in traversable added

--- src/test/scala/samples/TraversableTest.scala
+++ src/test/scala/samples/TraversableTest.scala
@@ -170,4 +170,37 @@ class TraversableTest {
     assertEquals(6, result.size)
     assertEquals(true, result.forall((f: String) => f.length == 1))
   }
+
+  @Test
+  def groupByExample() = {
+    //given
+    val list: Traversable[String] = List("a", "b", "c")
+
+    //when
+    val map: Map[Int, Traversable[String]] = list.groupBy((f: String) => f.length)
+
+    //then
+    assertEquals(1, map.size)
+    assertEquals(list, map.get(1).get)
+  }
+
+  @Test
+  def collectExampleTest() = {
+    //given
+    val list: Traversable[Any] = List("1", 2, 3.43, true, false)
+
+    //when
+    val collect: Traversable[Int] = list.collect(convertFn)
+
+    //then
+    assertEquals(5, collect.size)
+    assertEquals(Traversable(1, 2, 3, 1, 0), collect)
+  }
+
+  val convertFn: PartialFunction[Any, Int] = {
+    case i: Int => i;
+    case s: String => s.toInt;
+    case b: Boolean => if (b) 1 else 0;
+    case d: Double => d.toInt;
+  }
 }

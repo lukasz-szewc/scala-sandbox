81f1c7335ce1bf010012310e57d7f039990743f6
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-04-13 15:20:44 +0200

    split test methods into separate classes

--- src/test/scala/traversable/TraversableGenericTest.scala
+++ src/test/scala/traversable/TraversableGenericTest.scala
@@ -1,11 +1,11 @@
-package samples
+package traversable
 
 import java.util
 
 import org.junit.Assert._
 import org.junit.Test
 
-class TraversableTest {
+class TraversableGenericTest {
 
   @Test
   def createdJustByTraversable() = {
@@ -94,21 +94,6 @@ class TraversableTest {
   }
 
   @Test
-  def copyToArrayTest() = {
-    //given
-    val list: Traversable[String] = List("a", "b", "c")
-    val destination = new Array[String](3)
-
-    //when
-    list.copyToArray(destination)
-
-    //then
-    assertEquals("a", destination(0))
-    assertEquals("b", destination(1))
-    assertEquals("c", destination(2))
-  }
-
-  @Test
   def headAndTailsTest() = {
     //given
     val list: Traversable[String] = List("a", "b", "c")
@@ -146,32 +131,6 @@ class TraversableTest {
   }
 
   @Test
-  def mapExampleTest() = {
-    //given
-    val list: Traversable[String] = List("a", "b", "c")
-
-    //when
-    val result: Traversable[Int] = list.map((f: String) => f.length)
-
-    //then
-    assertEquals(3, result.size)
-    assertEquals(true, result.forall((f: Int) => f == 1))
-  }
-
-  @Test
-  def flatMapExampleTest() = {
-    //given
-    val list: Traversable[String] = List("a", "b", "c")
-
-    //when
-    val result: Traversable[String] = list.flatMap((f: String) => Traversable(f.toLowerCase, f.toUpperCase))
-
-    //then
-    assertEquals(6, result.size)
-    assertEquals(true, result.forall((f: String) => f.length == 1))
-  }
-
-  @Test
   def groupByExample() = {
     //given
     val list: Traversable[String] = List("a", "b", "c")
@@ -184,23 +143,4 @@ class TraversableTest {
     assertEquals(list, map.get(1).get)
   }
 
-  @Test
-  def collectExampleTest() = {
-    //given
-    val list: Traversable[Any] = List("1", 2, 3.43, true, false)
-
-    //when
-    val collect: Traversable[Int] = list.collect(convertFn)
-
-    //then
-    assertEquals(5, collect.size)
-    assertEquals(Traversable(1, 2, 3, 1, 0), collect)
-  }
-
-  val convertFn: PartialFunction[Any, Int] = {
-    case i: Int => i;
-    case s: String => s.toInt;
-    case b: Boolean => if (b) 1 else 0;
-    case d: Double => d.toInt;
-  }
 }

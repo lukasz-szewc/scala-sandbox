9ec7c0f5ea53d0749524bd22674e61a0610dc56a
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-04-13 17:37:18 +0200

    add take and drop example

--- src/test/scala/traversable/TraversableSubcollectionsTest.scala
+++ src/test/scala/traversable/TraversableSubcollectionsTest.scala
@@ -42,4 +42,28 @@ class TraversableSubcollectionsTest {
     assertEquals(Traversable(), list.slice(2, 2))
   }
 
+
+  @Test
+  def takeTraversableTest() = {
+    //given
+    val list: Traversable[String] = Traversable("a", "b", "c", "d", "e")
+
+    //when + then
+    assertEquals(Traversable("a", "b", "c", "d", "e"), list.take(5))
+    assertEquals(Traversable("a", "b", "c"), list.take(3))
+    assertEquals(Traversable("a"), list.take(1))
+    assertEquals(Traversable(), list.take(0))
+  }
+
+  @Test
+  def dropTraversableTest() = {
+    //given
+    val list: Traversable[String] = Traversable("a", "b", "c", "d", "e")
+
+    //when + then
+    assertEquals(Traversable("a", "b", "c", "d", "e"), list.drop(0))
+    assertEquals(Traversable("c", "d", "e"), list.drop(2))
+    assertEquals(Traversable("e"), list.drop(4))
+    assertEquals(Traversable(), list.drop(5))
+  }
 }

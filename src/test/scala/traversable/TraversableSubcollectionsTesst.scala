5ef40cf864e8273f32a8f80f63bd7d2b1f9a1283
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-04-13 16:43:59 +0200

    add tests of sub collection methods inside Traversable

---
+++ src/test/scala/traversable/TraversableSubcollectionsTesst.scala
@@ -0,0 +1,33 @@
+package traversable
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TraversableSubcollectionsTesst {
+
+  @Test(expected = classOf[UnsupportedOperationException])
+  def nilTraversableTail() = {
+    //given + when + then
+    val tail: List[Nothing] = Nil.tail
+  }
+
+  @Test(expected = classOf[UnsupportedOperationException])
+  def emptyTraversableTail() = {
+    //given + when + then
+    val tail: Traversable[Nothing] = Traversable().tail
+  }
+
+  @Test
+  def ordinaryTraversableTest() = {
+    //given
+    val list: Traversable[String] = Traversable("a", "b", "c")
+
+    //when + then
+    assertEquals(List("b", "c"), list.tail)
+    assertEquals(List("a", "b"), list.init)
+    assertEquals(List("b"), list.tail.init)
+    assertEquals(List("c"), list.tail.tail)
+    assertEquals(List(), list.tail.tail.tail)
+  }
+
+}
